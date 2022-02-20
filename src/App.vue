278c83c43013f09cff48f33f3fbae8da22159bbb
furkan bora <96105426+furkanbora239@users.noreply.github.com>
2022-02-20 16:38:46 +0300

    Add files via upload
    remember 
    npm install
    npm run dev
    npm run build

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <bas></bas>
+    <hr>
+    <list></list>
+    <hr>
+    <foot></foot>
+  </div>
+</template>
+
+<script>
+import baslik from './baslik.vue'
+import list from './list.vue'
+import foot from './foot.vue'
+export default {
+  components:{
+    "bas": baslik,
+    "list": list,
+    "foot": foot
+  }
+  
+}
+</script>
+
+<style>
+body{
+  background: rgba(117, 116, 55, 0.404);
+}
+</style>
---
+++ src/baslik.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <header>
+                    <h1>{{header}}</h1>
+                </header>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            header: "Sunucu Durumu"
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
